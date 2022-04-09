8da5112c96e90610f3813ab95fe2d9c6f7ba6705
임또이 <sh663746@gmail.com>
2022-04-09 13:27:22 +0900

    index 파일 못 읽어옴

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -7,13 +7,22 @@ const FormView = Object.create(View)
 FormView.setup = function (el) {
   this.init(el)
   this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
+  this.resetEl = el.querySelector('[type=reset]')
   this.showResetBtn(false)
+  this.bindEvents()
   return this
 }
 
 FormView.showResetBtn = function (show = true) {
   this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function () {
+  this.inputEl.addEventListener('keyup', e => this.onkeyup(e))
+}
+
+FormView.onkeyup = function () {
+  this.showResetBtn(this.inputEl.value.length)
 }
 
 export default FormView
